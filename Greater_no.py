8e08076b033791cde748a06a902310bea1aaf8cc
anushika0609 <anushkasaini0609@gmail.com>
2025-01-28 19:51:06 +0530

    add new file

---
+++ Greater_no.py
@@ -0,0 +1,8 @@
+a = float(input("Enter first no. : "))
+b = float(input("Enter second no. : "))
+if(a>b):
+    print("First no. is greater.")
+elif(a==b):
+    print("Both no.s are equal.")
+else:
+    print("Second no. is greater.")
