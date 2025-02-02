a8c4553561f7214f7afc55f0d9bcc607acc4440b
anushika0609 <anushkasaini0609@gmail.com>
2025-02-02 11:33:28 +0530

    add new file

---
+++ Largest_no.py
@@ -0,0 +1,14 @@
+a = float(input("Enter first no. (a) : "))
+b = float(input("Enter second no. (b) : "))
+c = float(input("Enter third no. (c): "))
+
+if(a>b and a>c):
+    print("a is greater.")
+elif(b>a and b>c):
+    print("b is greater.")
+elif(c>a and c>b):
+    print("c is greater.")
+else:
+    print("a, b and c are equal.")
+
+        
